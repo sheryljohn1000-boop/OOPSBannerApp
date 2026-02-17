8822699ee7d20f0042649a458bd0a0988a94f342
sheryljohn1000-boop <sheryljohn1000@gmail.com>
2026-02-17 23:17:43 +0530

    Fixed error

--- UC4.java
+++ UC4.java
@@ -1,13 +1,13 @@
 public class UC4{
 	public static void main(String[] args) {
 		String[] lines =new String[7];
-		lines[0] = String.join(" *****    *****    ******    ****** "));
-        lines[1] = (String.join("*     *  *     *   *     *   *      "));
-        lines[2] = (String.join("*     *  *     *   *     *   *      "));
-        lines[3] = (String.join("*     *  *     *   ******    ****** "));
-        lines[4] = (String.join("*     *  *     *   *               *"));
-        lines[5] = (String.join("*     *  *     *   *               *"));
-        lines[6] = (String.join(" *****    *****    *         ****** "));
+		lines[0] = " *****    *****    ******    ****** ";
+        lines[1] = "*     *  *     *   *     *   *      ";
+        lines[2] = "*     *  *     *   *     *   *      ";
+        lines[3] = "*     *  *     *   ******    ****** ";
+        lines[4] = "*     *  *     *   *               *";
+        lines[5] = "*     *  *     *   *               *";
+        lines[6] = " *****    *****    *         ****** ";
 		for (String line : lines) {
 			System.out.println(line);
 		}
